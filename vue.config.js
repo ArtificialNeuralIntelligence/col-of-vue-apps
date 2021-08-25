8834d44f932823fa06210b6e9d9b8256990aca77
AshiquzzamanKhan <primeintegerslab@gmail.com>
2021-08-26 05:47:35 +0600

    Working clean slate for vue3
    Modified:
      -- package.json dependecies installed/updated
      -- public/favicon.ico tracked with git lfs
      -- .eslintrc.js edited to support prettier
      -- Home.vue & Hello.vue cleaning
      -- assets/*.png default deleted
    Added:
      -- .gitattributes to track git lfs files
      -- .prettierrc to resolve conflict with eslint
      -- vue.config.js for default page title
      -- _empty for keeping empty src/assets folder

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+// vue.config.js
+
+/**
+ * @type {import('@vue/cli-service').ProjectOptions}
+ */
+module.exports = {
+  // https://cli.vuejs.org/ru/config/#chainwebpack
+  chainWebpack: (config) => {
+    config.plugin("html").tap((args) => {
+      // eslint-disable-next-line no-param-reassign
+      args[0].title = "Title Goes here";
+      return args;
+    });
+  },
+};
