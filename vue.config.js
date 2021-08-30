e80aec680ca1400cf2ab8aba781a5bb66e4a1920
AshiquzzamanKhan <primeintegerslab@gmail.com>
2021-08-31 00:30:50 +0600

    Rebase dev
    dev branch reabsed to initial value to learn composition api

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,7 @@ module.exports = {
   chainWebpack: (config) => {
     config.plugin("html").tap((args) => {
       // eslint-disable-next-line no-param-reassign
-      args[0].title = "Title Goes here";
+      args[0].title = "Composition API";
       return args;
     });
   },
