9fa662c0ae21502b0807259473c43b588689560b
AshiquzzamanKhan <primeintegerslab@gmail.com>
2021-09-02 02:52:20 +0600

    404 Error redirection
    NotFound.vue added
    router/index.js 404 route added

---
+++ src/views/NotFound.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>The page you are looking for doesn't exist</h1>
+    <button @click="goHome">Go to home</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotFound",
+  setup() {
+    const goHome = () => {
+      this.$router.push({ name: "Home" });
+    };
+    return { goHome };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
